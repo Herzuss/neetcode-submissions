f0dd1d198ed123d6e65c5e3e5228124a5294dd29
Filip Herzog <filip.herzog05@gmail.com>
2026-07-17 19:17:58 +0200

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let output = []
+        for(let i = 0; i < nums.length; i++){
+            for(let j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] === target && i != j){
+                    output.push(i,j)
+                    return output
+                }
+            }
+        }
+        return output
+    }
+}
